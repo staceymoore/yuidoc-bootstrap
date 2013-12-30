76019487cc2f5a845df07e24d8780a978e36352b
Stacey Moore <staceymoore@users.noreply.github.com>
2013-12-29 23:01:47 -0800

    Add helper for home page content

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,4 +1,7 @@
+var fs, path, markdown;
 fs = require('fs');
+path = require("path");
+markdown = require( "markdown" ).markdown;
 
 module.exports = {
     publicClasses: function(context, options) {
@@ -38,5 +41,26 @@ module.exports = {
         }
 
         return ret;
+    },
+    homeContent : function () {
+        var config = path.join(process.cwd(), 'yuidoc');
+        if (fs.existsSync(config + '.json') === true) {
+            config = require(config);
+        }
+
+        var content = config.theme.home
+        if (typeof content === 'string' && content !== '') {
+            content = path.join(process.cwd(), content);
+        } else {
+            content = path.join(process.cwd(), 'README.md');
+        }
+       
+        if (fs.existsSync(content) === true) {  
+            content = fs.readFileSync(content, {encoding: "utf8"});
+            content = markdown.toHTML(content);
+        } else {
+            content = '';
+        }
+        return content;
     }
 };
